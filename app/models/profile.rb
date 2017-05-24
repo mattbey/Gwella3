e6fda0197e48bde456ef5d71be16edfa333a8a81
mattbey <matt.bey@gmail.com>
2017-05-23 22:18:26 -0500

    5/23/17

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ApplicationRecord
+
+  belongs_to(:video, :class_name => "Video", :foreign_key => "video_id")
+
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,6 @@
 class Video < ApplicationRecord
+  validates :video_id, :presence => true
+
+
+
 end
